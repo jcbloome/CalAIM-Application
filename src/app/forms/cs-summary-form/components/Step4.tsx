08f511d3667d8167087e7fef14a6ece37a037e8f
Jason Bloome <jason@carehomefinders.com>
2025-12-02 17:51:00 +0000

    still error!

--- src/app/forms/cs-summary-form/components/Step4.tsx
+++ src/app/forms/cs-summary-form/components/Step4.tsx
@@ -85,9 +85,9 @@ export default function Step4() {
                     render={({ field }) => (
                         <FormItem>
                         <FormLabel>Type of Location <span className="text-destructive">*</span></FormLabel>
-                        <Select onValueChange={field.onChange} value={field.value}>
+                         <Select onValueChange={field.onChange} value={field.value}>
                             <FormControl>
-                            <SelectTrigger><SelectValue placeholder="Select a location type" /></SelectTrigger>
+                                <SelectTrigger><SelectValue placeholder="Select a location type" /></SelectTrigger>
                             </FormControl>
                             <SelectContent>
                                 <SelectItem value="SNF">Skilled Nursing Facility</SelectItem>
