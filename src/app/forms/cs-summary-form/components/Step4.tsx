7c9d3e6eb0f20ab0be820fee114e213bdeb40f89
Jason Bloome <jason@carehomefinders.com>
2025-12-03 02:03:14 +0000

    Try fixing this error: `Runtime Error: Error: Maximum update depth excee

--- src/app/forms/cs-summary-form/components/Step4.tsx
+++ src/app/forms/cs-summary-form/components/Step4.tsx
@@ -79,36 +79,33 @@ export default function Step4() {
 
                <div className="space-y-4 p-4 border rounded-md mt-4">
                  <h3 className="font-medium text-base">ISP Assessment Location</h3>
-                 <FormField
+                  <FormField
                     control={control}
                     name="ispLocationType"
-                    render={({ field }) => {
-                        console.log('[Step4 Dropdown Log] Rendering with value:', field.value);
-                        return (
-                            <FormItem>
-                                <FormLabel>Type of Location <span className="text-destructive">*</span></FormLabel>
-                                <Select onValueChange={field.onChange} value={field.value ?? ''}>
-                                    <FormControl>
-                                        <SelectTrigger>
-                                        <SelectValue placeholder="Select a location type" />
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        <SelectItem value="SNF">Skilled Nursing Facility</SelectItem>
-                                        <SelectItem value="Home">Home</SelectItem>
-                                        <SelectItem value="Hospital">Hospital</SelectItem>
-                                        <SelectItem value="Sub-Acute">Sub-Acute</SelectItem>
-                                        <SelectItem value="Recuperative Care">Recuperative Care</SelectItem>
-                                        <SelectItem value="Unhoused">Unhoused</SelectItem>
-                                        <SelectItem value="RCFE/ARF">RCFE/ARF</SelectItem>
-                                        <SelectItem value="Other">Other</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                                <FormMessage />
-                            </FormItem>
-                        )
-                    }}
-                    />
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Type of Location <span className="text-destructive">*</span></FormLabel>
+                        <Select onValueChange={field.onChange} value={field.value ?? ''}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select a location type" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="SNF">Skilled Nursing Facility</SelectItem>
+                            <SelectItem value="Home">Home</SelectItem>
+                            <SelectItem value="Hospital">Hospital</SelectItem>
+                            <SelectItem value="Sub-Acute">Sub-Acute</SelectItem>
+                            <SelectItem value="Recuperative Care">Recuperative Care</SelectItem>
+                            <SelectItem value="Unhoused">Unhoused</SelectItem>
+                            <SelectItem value="RCFE/ARF">RCFE/ARF</SelectItem>
+                            <SelectItem value="Other">Other</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                  <FormField
                     control={control}
                     name="ispCopyCurrent"
