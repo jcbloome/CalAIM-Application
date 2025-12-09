186ae30a999c815a25f5be19454512a847034c2e
Jason Bloome <jason@carehomefinders.com>
2025-12-09 13:57:20 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,11 +30,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // This is the correct way to allow cross-origin requests in the Next.js dev server.
+  allowedDevOrigins: [
+      "https://6000-firebase-studio-1763747953373.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev"
+  ],
   experimental: {
-    // This is the correct way to allow cross-origin requests in the Next.js dev server.
-    allowedDevOrigins: [
-        "https://6000-firebase-studio-1763747953373.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev"
-    ]
   }
 };
 
