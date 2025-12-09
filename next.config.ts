3ffb011fac6bc56656ca387eeef5de78ac576a3e
Jason Bloome <jason@carehomefinders.com>
2025-12-09 02:49:47 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,11 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  devIndicators: {
+    allowedDevOrigins: [
+      "https://6000-firebase-studio-1763747953373.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev"
+    ]
+  }
 };
 
 export default nextConfig;
