7ae85171a4597e49cbdd0fe7ea0c3dee7e4085b9
Jason Bloome <jason@carehomefinders.com>
2025-12-17 17:07:54 +0000

    also members eligible for the non-medical out of home care payment (NMOH

--- src/app/forms/waivers/page.tsx
+++ src/app/forms/waivers/page.tsx
@@ -275,7 +275,7 @@ function WaiversFormComponent() {
 
                             <Section title="Room & Board Obligation" icon={Home}>
                                 <p>The MCP member is responsible for paying the RCFE the “room and board” and the MCP is responsible for paying the RCFE the “assisted living” portion.</p>
-                                <p>For members eligible for SSI/SSP and the 2026 Non-Medical Out of Home Care payment (NMOHC), SSI/SSP is bumped up to $1,626.07. The member usually retains $182 for personal needs expenses and the RCFE receives the $1,444.07 balance as payment for “room and board”.</p>
+                                <p>For members eligible for SSI/SSP and the 2026 Non-Medical Out of Home Care payment (NMOHC), SSI/SSP is bumped up to $1,626.07. The member usually retains $182 for personal needs expenses and the RCFE receives the $1,444.07 balance as payment for “room and board”. Also, members eligible for the NMOHC will pay at least $1,447.00 to the RCFE. Members who receive more than this amount can pay more for "room and board" for a private room or to open up RCFEs in more expensive areas.</p>
                                 <p>For example, Mr. Johnson is eligible for NMOHC and receives $500/month. The NMOHC will bump up the payment to the RCFE to $1,444.07 for “room and board” and he will retain $182 for personal needs expenses.</p>
                                 <p>Members not eligible for the NMOHC will still have a “room and board” obligation but the amount could be flexible depending on the RCFE and the assessed tiered level.</p>
                                 <p>Members who cannot pay any “room and board” portion or who do not have families who could pay this portion are not eligible for the CS since program requirements mandate a "room and board” payment from the member (or their family).</p>
@@ -385,5 +385,3 @@ export default function WaiversPage() {
         </Suspense>
     );
 }
-
-    
